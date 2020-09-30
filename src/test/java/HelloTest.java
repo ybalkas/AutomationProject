2dc9b78e36e1444887501f7a8ee63ac4c2154b4d
Yusuf Balkas <ybalkas@gmail.com>
2020-09-30 21:43:54 +0100

    cdsfasdfs

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("HelloTest");
         System.out.println("changes");
+        System.out.println("hdshfsdfsdf");
     }
 }
