c0ffe423ce6a5f0aa6a2f852faa788200dd0d5d5
Yusuf Balkas <ybalkas@gmail.com>
2020-09-30 21:41:12 +0100

    another added

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello class");
         System.out.println("changes are done");
+        System.out.println("another change");
     }
 }
